73e25e5097c0c48f2758300911181352eee6b471
Raymond Cornely <cornely06@gmail.com>
2021-06-04 10:58:26 -0400

    Add move to gameboard as space is clicked by user

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,14 +1,33 @@
 var Gameboard = (function() {
-    const emptyBoard = function() {
+    let piece = 'X';
+
+    const switchPiece = function() {
+        if (piece == 'X') {
+            piece = 'O';
+        } else {piece = 'X'}
+    }
+
+    const setChoice = function(e) {
+        if (!e.target.textContent) {
+            e.target.textContent = piece;
+        } else {
+            console.log('error: occupied space');
+        }
+        switchPiece();
+    }
+
+    const reset = function() {
         let gameboard = document.querySelector('#gameboard')
         for (let i = 1; i <= 9; i++) {
             let cell = document.createElement('div');
             cell.dataset.index = i;
             cell.classList.add('cell');
+            cell.addEventListener('mousedown', setChoice);
             gameboard.appendChild(cell);
         }
+        piece = 'X';
     };
-    return { emptyBoard };
+    return { reset };
 })();
 
-Gameboard.emptyBoard();
+Gameboard.reset();
